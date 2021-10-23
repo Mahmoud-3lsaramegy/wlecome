2837804906913f49e4ca7e893ff5f4f5e1d11fff
Mahmoud-3lsaramegy <elsaramegymahmoud404@gmail.com>
2021-10-23 17:56:58 +0200

    frist code

---
+++ android/app/src/main/kotlin/com/example/wlecome/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wlecome
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/hiscreen.dart
@@ -0,0 +1,105 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class HI_Screen extends StatelessWidget {
+  const HI_Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: const Color.fromARGB(200, 58, 53, 48),
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 108.1,
+                ),
+                child: Center(
+                  child: SizedBox(
+                    width: 186,
+                    height: 227.27,
+                    child: Image.asset("images/logo.png"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 79.6,
+                ),
+                child: Center(
+                  child: Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontSize: 36,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 100,
+                ),
+                child: Center(
+                  child: Text(
+                    "Hi Frist Widget by <3lsaramegy>I Used Flutter",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: 20,
+                right: 15,
+                left: 15,
+              ),
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("LiKE"),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red),
+                    // shape: MaterialStateProperty.all<OutlinedBorder>(),
+                  ),
+                ),
+                width: double.infinity,
+                height: 49,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+/*
+          Container(
+            color: const Color.fromARGB(200, 58, 53, 48),
+          ),
+          Column(
+            children: [Image.asset("images/clogo.png"
+            */
